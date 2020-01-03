7e89e5b13bd79a14cdfc9f50c8181df058b45422
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2020-01-03 15:10:23 -0300

    Cadastro na aplicação

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,22 +1,48 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
-import { signInSucess } from './actions';
+import { signInSucess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
-  const response = yield call(api.post, '/sessions', {
-    email,
-    password,
-  });
-  const { token, user } = response.data;
-  if (!user.provider) {
-    console.tron.error('Usuário não é prestador');
-    return;
+  try {
+    const { email, password } = payload;
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
+    const { token, user } = response.data;
+    if (!user.provider) {
+      toast.error('Usuário não é prestador');
+      yield put(signFailure());
+      return;
+    }
+    yield put(signInSucess(token, user));
+    history.push('/dashboard');
+  } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados');
+    yield put(signFailure());
   }
-  yield put(signInSucess(token, user));
-  history.push('/dashboard');
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+    yield call(api.post, '/users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique seus dados');
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
